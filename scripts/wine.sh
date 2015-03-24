ddde340e99d66b6d9abdd7ce5d4715879ba981cf
Christoph Winkler <cw@mpf-medical.de>
2015-03-24 10:49:15 +0100

    install wine packages

--- scripts/wine.sh
+++ scripts/wine.sh
@@ -1,2 +1,6 @@
-dpkg --add-architecture i386
-apt-get -y install wine wine32 lib32gcc1
+#dpkg --add-architecture i386
+#apt-get -y install wine wine32
+
+add-apt-repository ppa:ubuntu-wine/ppa
+apt-get update
+apt-get install wine1.7
