e859081f4a09b20be820a6d6a26460eba8b1eba7
Christoph Winkler <cw@mpf-medical.de>
2015-03-24 14:16:29 +0100

    moved dependency installation to base.sh

--- scripts/base.sh
+++ scripts/base.sh
@@ -1,2 +1,3 @@
 apt-get update
 apt-get -y upgrade
+apt-get -y install build-essential git
--- scripts/wine.sh
+++ scripts/wine.sh
@@ -1,6 +1,5 @@
-apt-get -y install git build-essential
-
 git clone git://source.winehq.org/git/wine.git /tmp/wine
+
 cd /tmp/wine
 
 ./configure --without-x --enable-win64
