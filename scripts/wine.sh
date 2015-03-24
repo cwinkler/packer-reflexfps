73e3855685ad9b84fc5203a9ea8e37d5e63bf600
Christoph Winkler <cw@mpf-medical.de>
2015-03-24 16:23:08 +0100

    install flex

--- scripts/wine.sh
+++ scripts/wine.sh
@@ -4,9 +4,26 @@ set -e
 
 apt-get -y install build-essential git
 
+# install flex
+
+mkdir -p /tmp/flex
+cd /tmp/flex
+
+wget http://sourceforge.net/projects/flex/files/latest/download -O flex.tar.bz2
+
+tar xvjf flex.tar.bz2
+
+cd flex
+
+./configure && make && make install
+
+
+
 git clone git://source.winehq.org/git/wine.git /tmp/wine
 
 cd /tmp/wine
+
+git checkout tags/wine-1.6.2
 
 ./configure --without-x --enable-win64
 make
