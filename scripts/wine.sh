b1783c081ff944d832a61a387112122674f8acd2
Christoph Winkler <cw@mpf-medical.de>
2015-03-24 10:53:09 +0100

    added -y switch

--- scripts/wine.sh
+++ scripts/wine.sh
@@ -1,6 +1,6 @@
 #dpkg --add-architecture i386
 #apt-get -y install wine wine32
 
-add-apt-repository ppa:ubuntu-wine/ppa
+add-apt-repository -y ppa:ubuntu-wine/ppa
 apt-get update
 apt-get install wine1.7
