8d4049b17551db0a0210287ca056ae40e49a6247
Christoph Winkler <cw@mpf-medical.de>
2015-03-24 11:20:43 +0100

    compile wine from source

--- scripts/wine.sh
+++ scripts/wine.sh
@@ -1,6 +1,6 @@
-#dpkg --add-architecture i386
-#apt-get -y install wine wine32
+apt-get -y install git
 
-add-apt-repository -y ppa:ubuntu-wine/ppa
-apt-get update
-apt-get install wine1.7
+git clone git://source.winehq.org/git/wine.git /tmp/wine
+cd /tmp/wine
+
+./configure --without-x
