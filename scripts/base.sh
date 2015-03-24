6d0e406693b40cbd097a673677ff7760663c29a2
Christoph Winkler <cw@mpf-medical.de>
2015-03-24 15:57:15 +0100

    try dist-upgrade before installing

--- scripts/base.sh
+++ scripts/base.sh
@@ -2,10 +2,15 @@
 
 set -e
 
-grep -r ^deb\  /etc/apt/sources.list
-
-apt-cache policy build-essential
-
-apt-get update
-apt-get -y install build-essential git vim curl
 apt-get -y upgrade
+apt-get -y dist-upgrade
+apt-get update
+
+
+#grep -r ^deb\  /etc/apt/sources.list
+
+#apt-cache policy build-essential
+
+
+apt-get -y install build-essential git vim curl
+#apt-get -y upgrade
