042f00b4a9f14461b12bb5c5c62eb9990d423dc9
Christoph Winkler <cw@mpf-medical.de>
2015-03-25 09:09:33 +0100

    added steam/reflex scripts

--- scripts/wine.sh
+++ scripts/wine.sh
@@ -2,24 +2,4 @@
 
 set -e
 
-# install dependencies
-apt-get -y install wine1.7 winetricks
-#apt-get -y install build-essential git autoconf 
-
-# install flex
-#git clone git://git.code.sf.net/p/flex/flex /tmp/flex && cd /tmp/flex
-#git checkout tags/flex-2.5.39
-
-#./autogen.sh
-
-#ls -alh
-
-#make && make install
-
-# install wine
-#git clone git://source.winehq.org/git/wine.git /tmp/wine && cd /tmp/wine
-#git checkout tags/wine-1.6.2
-
-#./configure --without-x --enable-win64
-
-#make && make install
+apt-get -y install wine1.7 winetricks xvfb
