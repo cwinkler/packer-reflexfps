9743396a10c70089f3b7bf71d015abe0046111ec
Christoph Winkler <cw@mpf-medical.de>
2015-03-26 14:18:17 +0100

    added upstart script

--- scripts/steam.sh
+++ scripts/steam.sh
@@ -10,3 +10,7 @@ useradd --gid 2000 --uid 2000 -d /var/steam -m steam
 su --login steam --shell /bin/bash --command "wget http://media.steampowered.com/installer/steamcmd_linux.tar.gz -O /tmp/steamcmd.tar.gz"
 su --login steam --shell /bin/bash --command "mkdir -p /var/steam/steamcmd"
 su --login steam --shell /bin/bash --command "tar xvzf /tmp/steamcmd.tar.gz -C /var/steam/steamcmd"
+
+cp /tmp/reflexfps.conf /etc/init/reflexfps.conf
+
+chmod 0644 /etc/init/reflexfps.conf
